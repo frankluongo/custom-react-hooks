c05f56c2e972cf6ceaf08a7d4b04a2847636a74b
Frank Luongo <fluongo@oreilly.com>
2020-04-28 08:25:02 -0400

    16: useMeasure

--- src/Components/Hover/Hover.jsx
+++ src/Components/Hover/Hover.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import {Card} from "../../Elements"
-import { useHover } from '../../Hooks/useHover'
+import React from "react";
+import { Card } from "../../Elements";
+import { useHover, useMeasure } from "../../Hooks";
 
 const Hover = ({ imgSrc }) => {
-
   const [isHovered, bind] = useHover();
+  const [{ ref }, bounds] = useMeasure();
+  console.log(ref, bounds);
 
   return (
-    <Card {...bind} style={{ background: "var(--black)" }}>
-    <h3>
-      {isHovered ? "Err' day I'm Hoverin" : "Hover Over This Card"}
-    </h3>
-    <img alt="Card Description" src={imgSrc} />
-  </Card>
-  )
-}
+    <Card {...bind} style={{ background: "var(--black)" }} ref={ref}>
+      <h3>{isHovered ? "Err' day I'm Hoverin" : "Hover Over This Card"}</h3>
+      <img alt="Card Description" src={imgSrc} />
+    </Card>
+  );
+};
 
-export default Hover
+export default Hover;
